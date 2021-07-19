ff0095a96f8bbc7b4afaf18996985ecf53ff51cc
John Norris <john@norricorp.f9.co.uk>
2021-07-19 06:29:22 +0100

    Changes for svelte-kit from https://blog.stranianelli.com/svelte-et-github-english/

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,22 @@
 import preprocess from 'svelte-preprocess';
 import node from '@sveltejs/adapter-static';
 
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		//adapter: node(),
-		adapter: node(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',  // path to public directory
+			assets: 'build',  // path to public directory
+			fallback: null
+		})
 	}
 };
 
 export default config;
+
