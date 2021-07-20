f6795f57e7c3fb2f7ea6a6a3681069436011be0a
John Norris <john@norricorp.f9.co.uk>
2021-07-20 08:40:55 +0100

    replace public folder with build

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 import { publish } from 'gh-pages';
 
 publish(
-	'public', // path to public directory
+	'build', // path to public directory
 	{
 		branch: 'gh-pages',
 		repo: 'https://github.com/norricorp/norricorp.github.io.git', // Update to point to your repository
